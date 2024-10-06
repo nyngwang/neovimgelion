aa5d1d4cc6a9ab282760df32c7fe6f067bea3f2f
SharieRhea <sharierhea@gmail.com>
2024-10-06 17:46:38 -0400

    refact: tweak colors to improve readability

--- lua/lualine/themes/nvimgelion.lua
+++ lua/lualine/themes/nvimgelion.lua
@@ -7,8 +7,8 @@ local colors = {
   purple       = '#c51af9',
   bg           = '#16161d',
   orange       = '#fc735d',
-  lightgray    = '#6D6673',
-  inactivegray = '#686858',
+  lightgray    = '#484848',
+  inactivegray = '#696969',
 }
 return {
   normal = {
@@ -17,24 +17,24 @@ return {
     c = {bg = colors.orange, fg = colors.bg}
   },
   insert = {
-    a = {bg = colors.yellow, fg = colors.black, gui = 'bold'},
+    a = {bg = colors.black, fg = colors.yellow, gui = 'bold'},
     b = {bg = colors.lightgray, fg = colors.white},
-    c = {bg = colors.lightgray, fg = colors.white}
+    c = {bg = colors.yellow, fg = colors.bg}
   },
   visual = {
-    a = {bg = colors.purple, fg = colors.black, gui = 'bold'},
+    a = {bg = colors.black, fg = colors.purple, gui = 'bold'},
     b = {bg = colors.lightgray, fg = colors.white},
-    c = {bg = colors.inactivegray, fg = colors.black}
+    c = {bg = colors.purple, fg = colors.bg}
   },
   replace = {
-    a = {bg = colors.red, fg = colors.black, gui = 'bold'},
+    a = {bg = colors.black, fg = colors.red, gui = 'bold'},
     b = {bg = colors.lightgray, fg = colors.white},
-    c = {bg = colors.black, fg = colors.white}
+    c = {bg = colors.red, fg = colors.bg}
   },
   command = {
-    a = {bg = colors.green, fg = colors.black, gui = 'bold'},
+    a = {bg = colors.black, fg = colors.green, gui = 'bold'},
     b = {bg = colors.lightgray, fg = colors.white},
-    c = {bg = colors.inactivegray, fg = colors.black}
+    c = {bg = colors.green, fg = colors.bg}
   },
   inactive = {
     a = {bg = colors.orange, fg = colors.bg, gui = 'bold'},
